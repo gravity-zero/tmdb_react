f52aa58c4314ed5a92c5eb60721f646e740ddd50
gravity-zero <gravity.neo@gmail.com>
2020-11-18 11:24:25 +0100

    add input in Homepage component

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,16 @@
+import classnames from 'classnames';
+
+import './Homepage.css';
+
+function Homepage(props) {
+  const filter = "";
+
+  return (
+    <div className="body">
+      <div className="input">
+        <label htmlFor="filter">Recherche par Nom de film</label>
+        <input type="text" id="name" name="name" onChange></input>
+      </div>
+    </div>
+  )
+}
