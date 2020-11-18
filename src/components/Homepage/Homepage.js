a23ef587fb1b5cbe409a3c3eca0b6428f8e39370
UgoRicher <67463092+UgoRicher@users.noreply.github.com>
2020-11-18 16:50:18 +0100

    add search by title with api tmdb from value input

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,12 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Homepage.css';
+import { getMoviesByName } from '../../services/titles/titles';
 
 function Homepage() {
   //const filter = "";
   const [value, setValue] = useState({
      title: '' 
     });
+
+    const [movies, setMovies] = useState({
+      total_results: '',
+      results: []
+    });
+
 
   function handleChange(event) {
     setValue({
@@ -17,12 +24,20 @@ function Homepage() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(value.title);
+    console.log(handleSubmit)
+    getMoviesByName(value.title)
+        .then(data => {
+            setMovies(data)
+        console.log(useEffect)
+        })
   }
 
+console.log(movies)
+
+
   return (
-    <form onSubmit={handleSubmit}>
     <div className="body">
+    <form onSubmit={handleSubmit}>
       <div className="input">
         <label>
           Recherche par Nom de film
@@ -36,8 +51,16 @@ function Homepage() {
         <input type="submit" />
         </label>
       </div>
-    </div>
     </form>
+    <div className="Movies">
+        <h1>Total de résultat: {movies.results.length}</h1>
+        <ul>
+        { movies.results.map((movie) =>
+            <li value={movie.id}>{movie.title}</li>
+        )}
+        </ul>
+    </div>
+    </div>
   )
 }
 
