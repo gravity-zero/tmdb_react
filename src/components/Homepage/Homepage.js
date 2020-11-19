c8451f73f9eaf8c660c8215312bf4ec8a34dea5e
Nathan <nathan.provost78990@gmail.com>
2020-11-19 15:35:08 +0100

    Update Homepage.js

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import './Homepage.scss';
-import Header from '../Header/Header';
 import Card from '../Card/Card';
-import { BrowserRouter} from 'react-router-dom';
 
 function Homepage(props) {
 
